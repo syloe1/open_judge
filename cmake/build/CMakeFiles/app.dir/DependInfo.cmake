e8eb3b7c515155e9b038e2882c96c3d5c7d5ff8e
syloe1 <3337976147@qq.com>
2024-07-26 22:31:56 +0800

    Add cmake folder

---
+++ cmake/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wk/Desktop/open_judge/cmake/Proxy.cpp" "CMakeFiles/app.dir/Proxy.cpp.o" "gcc" "CMakeFiles/app.dir/Proxy.cpp.o.d"
+  "/home/wk/Desktop/open_judge/cmake/RealSubject.cpp" "CMakeFiles/app.dir/RealSubject.cpp.o" "gcc" "CMakeFiles/app.dir/RealSubject.cpp.o.d"
+  "/home/wk/Desktop/open_judge/cmake/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/Proxy.cpp.o"
+  "CMakeFiles/app.dir/Proxy.cpp.o.d"
+  "CMakeFiles/app.dir/RealSubject.cpp.o"
+  "CMakeFiles/app.dir/RealSubject.cpp.o.d"
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
